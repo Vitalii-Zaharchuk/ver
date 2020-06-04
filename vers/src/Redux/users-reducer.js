2a538206c5ac9ef81f012a0722bf99ae96ce2b5a
Vitalii-Zaharchuk <vitaliy.zakharchuk19@gmail.com>
2020-06-04 23:05:06 +0300

    class

--- vers/src/Redux/users-reducer.js
+++ vers/src/Redux/users-reducer.js
@@ -3,14 +3,7 @@ const UNFOLLOW_AC = 'UNFOLLOWAC'
 const SET_USERSAC = 'SETUSERSAC'
 let initialState ={
     users:[
-        {id: 1,photoAva: 'https://www.maxim.com/.image/t_share/MTU3NzI3MTA3OTM2NDI5MDYx/gettyimages-110307887.jpg',
-            firstName: 'Dima',status: 'I am boss', location: {city:'Moskow',country:'Russia'},follow:true},
-        {id: 2,photoAva: 'https://www.maxim.com/.image/t_share/MTU3NzI3MTA3OTM2NDI5MDYx/gettyimages-110307887.jpg',
-            firstName: 'Sasha',status: 'I like football', location:{city:'Kiev',country:'Ukraine'} ,follow:false},
-        {id: 3,photoAva: 'https://www.maxim.com/.image/t_share/MTU3NzI3MTA3OTM2NDI5MDYx/gettyimages-110307887.jpg',
-            firstName: 'Igor',status: 'I like travell', location: {city:'Belin',country:'German'},follow:true},
-        {id: 4,photoAva: 'https://www.maxim.com/.image/t_share/MTU3NzI3MTA3OTM2NDI5MDYx/gettyimages-110307887.jpg',
-            firstName: 'Misha',status: 'I am oss too', location:{city:'Varshawa',coutry:'Poland'} ,follow:false}
+        
     ]
 }
 const usersReducer = (state= initialState,action) =>{
@@ -38,9 +31,9 @@ const usersReducer = (state= initialState,action) =>{
                     return u;
                 })
             }
-         case SET_USERSAC:
-             return{...state,users:[...state.users,action.users]}  
-            
+         case SET_USERSAC:{
+             return{...state,users:[...state.users,...action.users]}  
+         }
         default:
             return state;  
     }
@@ -56,5 +49,5 @@ export let UNFOLLOWAC = (userId) =>{
     }
 }
 
-export let SETUSERSAC = (users) =>({type:SETUSERSAC,users})
+export let SETUSERSAC = (users) =>({type:'SETUSERSAC',users})
 export default usersReducer;
--- vers/src/component/Users/Users.js
+++ vers/src/component/Users/Users.js
@@ -1,35 +1,51 @@
 import React from 'react';
 import s from './Users.module.css'
-let Users = (props) =>{
+import * as axios from 'axios';
+import usersPhoto from '../../image/usersImage.png'
+class Users extends React.Component {
+    constructor(props){
+        super(props)
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response =>{ 
+        
+            this.props.setUsers(response.data.items)
+        })
+    }
+    render() {
+     
+        return (
 
-    return(
-        <div>
-            {props.users.map(u =><div key={u.id} className={s.users}>
-                <div className={s.ava}>
-                    <img src={u.photoAva}/>
-                    {u.follow? <button onClick={() =>{props.unfollow(u.id)}}>Unfollow</button> : <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
-                </div>
+            <div>
                 
-                <div className={s.about}>
-                    <div className={s.name}>
-                        <div>{u.firstName}</div>
-                        <div>{u.status}</div>
+                {this.props.users.map(u => <div key={u.id} className={s.users}>
+                    <div className={s.ava}>
+                        <img src={u.photos.small != null ? u.photos.small : usersPhoto} />
+                        {u.follow ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button> : <button onClick={() => {this.props.follow(u.id) }}>Follow</button>}
                     </div>
-                    <div className={s.local}>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+
+                    <div className={s.about}>
+                        <div className={s.name}>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </div>
+                        <div className={'s.local'}>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
+
+                        </div>
+
 
                     </div>
-                
-
-                </div>
 
 
 
 
 
-            </div> )}
-        </div>
-    )
+                </div>)}
+            </div>
+        )
+    }
 }
+
+
+
 export default Users;
